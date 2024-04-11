fa2417c587b7200ea8fcb32b25e6bf29850f0928
= <undeliverable@example.com>
2024-04-11 22:25:35 +0900

    Refactor for packaging reasons

--- alana/alana.py
+++ alana/alana.py
@@ -1,4 +1,4 @@
-from color import log, red, blue, green, yellow, cyan
+from alana.color import log, red, blue, green, yellow, cyan
 import alana.prompt as prompt
 from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen
 from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot
