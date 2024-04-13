88dedc3f6e53dbee10f8c959fc797c01e10dc585
= <undeliverable@example.com>
2024-04-14 00:40:31 +0900

    Don't enable `prompt.pretty_print` yet (it's too buggy)

--- alana/alana.py
+++ alana/alana.py
@@ -1,3 +1,3 @@
 from alana.color import red, blue, green, yellow, cyan
-from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt, pretty_print
+from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt
 from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot
