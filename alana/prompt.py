4d73fef1054692ca8c8b84ac432caf5e6b217f66
= <undeliverable@example.com>
2024-04-13 18:18:58 +0900

    Copy imports in `alana.py` to `__init__.py`.
    `alana.py` may be deprecated in the future.
    
    Other minor changes.

--- alana/prompt.py
+++ alana/prompt.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from alana.color import red
 import re
 
 def get_xml_pattern(tag: str):
@@ -13,21 +14,25 @@ def get_xml(tag: str, content: str) -> List[str]:
     matches = re.findall(pattern, content, re.DOTALL)
     return matches
 
-def grab(tag: str, content: str) -> List[str]:
-    return get_xml(tag, content)
+DEFAULT_MODEL = "sonnet" # Sonnet so fast!
 
-def xml(tag: str, content: str) -> List[str]:
-    return get_xml(tag, content)
+def create_message(user: str, system: str = "", model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens = 1024):
+    raise NotImplementedError
 
-def gen(user: str, system: str = "", model: str = "opus", api_key: Optional[str] = None, max_tokens = 1024) -> str:
+def gen(user: str, system: str = "", model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens = 1024) -> str:
     models = {
         'opus' : 'claude-3-opus-20240229',
         'sonnet' : 'claude-3-sonnet-20240229',
-        'haiku' : 'claude-3-haiku-0307'
+        'haiku' : 'claude-3-haiku-0307',
+        'claude-2.1' : 'claude-2.1',
+        'claude-2.0' : 'claude-2.0',
+        'claude-instant-1.2' : 'claude-instant-1.2'
     }
-    backend = models['opus']
+    backend = models[DEFAULT_MODEL]
     if model in models:
         backend = models[model]
+    else:
+        red(f"gen() -- Caution! model string not recognized; reverting to {DEFAULT_MODEL=}.") # TODO: C'mon we can do better error logging than this
 
     import os
     from anthropic import Anthropic
@@ -35,7 +40,6 @@ def gen(user: str, system: str = "", model: str = "opus", api_key: Optional[str]
     if api_key is None:
         api_key = os.environ.get("ANTHROPIC_API_KEY")
     client = Anthropic(
-        # This is the default and can be omitted
         api_key=api_key,
     )
 
@@ -50,9 +54,10 @@ def gen(user: str, system: str = "", model: str = "opus", api_key: Optional[str]
         system=system,
         model=backend,
     )
+    red(message)
     return message.content[0].text
 
-def gen_examples_list(instruction: str, n_examples: int = 5, model: str = "opus") -> List[str]:
+def gen_examples_list(instruction: str, n_examples: int = 5, model: str = "sonnet", api_key: Optional[str] = None, max_tokens: int = 1024) -> List[str]:
     system = """You are a prompt engineering assistant tasked with generating few-shot examples given a task.
 
     Your user would like to accomplish a specific task. His/her description of the task will be enclosed in <description></description> XML tags.
@@ -68,22 +73,45 @@ def gen_examples_list(instruction: str, n_examples: int = 5, model: str = "opus"
 
     Before generating your examples, feel free to think out loud using <thinking></thinking> XML tags.
     """.format(instruction=instruction)
-    model_output = gen(user=user, system=system, model=model)
+    model_output = gen(user=user, system=system, model=model, api_key=api_key, max_tokens=max_tokens)
     return get_xml('example', model_output)
 
-def gen_examples(instruction: str, n_examples: int = 5, model: str = "opus") -> str:
-    examples = gen_examples_list(instruction=instruction, n_examples=n_examples, model=model)
+def gen_examples(instruction: str, n_examples: int = 5, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> str:
+    examples = gen_examples_list(instruction=instruction, n_examples=n_examples, model=model, api_key=api_key, max_tokens=max_tokens)
     formatted_examples = "\n<examples>\n<example>" + '</example>\n<example>'.join(examples) + "</example>\n</examples>"
     return formatted_examples
 
-def n_shot_list(instruction: str, n_examples: int = 5, model: str = "opus"):
-    return gen_examples_list(instruction=instruction, n_examples=n_examples, model=model)
+def prompt_gen(instruction: str, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> str:
+    meta_system_prompt = """You are a prompt engineering assistant. Your task is to generate a 
 
-def n_shot(instruction: str, n_examples: int = 5, model: str = "opus"):
-    return gen_examples(instruction=instruction, n_examples=n_examples, model=model)
+    Your user would like to accomplish a specific task. His/her description of the task will be enclosed in <description></description> XML tags.
+    """
+    meta_prompt = """
 
-def few_shot_list(instruction: str, n_examples: int = 5, model: str = "opus"):
-    return gen_examples_list(instruction=instruction, n_examples=n_examples, model=model)
+    """
+    return gen(meta_prompt, meta_system_prompt, model=model, api_key=api_key, max_tokens=max_tokens)
 
-def few_shot(instruction: str, n_examples: int = 5, model: str = "opus"):
-    return gen_examples(instruction=instruction, n_examples=n_examples, model=model)
+# Aliases!
+def grab(tag: str, content: str) -> List[str]:
+    """Alias for get_xml"""
+    return get_xml(tag, content)
+
+def xml(tag: str, content: str) -> List[str]:
+    """Alias for get_xml"""
+    return get_xml(tag, content)
+
+def n_shot_list(instruction: str, n_examples: int = 5, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> List[str]:
+    """Alias for gen_examples_list"""
+    return gen_examples_list(instruction=instruction, n_examples=n_examples, model=model, api_key=api_key, max_tokens=max_tokens)
+
+def n_shot(instruction: str, n_examples: int = 5, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> str:
+    """Alias for gen_examples"""
+    return gen_examples(instruction=instruction, n_examples=n_examples, model=model, api_key=api_key, max_tokens=max_tokens)
+
+def few_shot_list(instruction: str, n_examples: int = 5, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> List[str]:
+    """Alias for gen_examples_list"""
+    return gen_examples_list(instruction=instruction, n_examples=n_examples, model=model, api_key=api_key, max_tokens=max_tokens)
+
+def few_shot(instruction: str, n_examples: int = 5, model: str = DEFAULT_MODEL, api_key: Optional[str] = None, max_tokens: int = 1024) -> str:
+    """Alias for gen_examples"""
+    return gen_examples(instruction=instruction, n_examples=n_examples, model=model, api_key=api_key, max_tokens=max_tokens)
