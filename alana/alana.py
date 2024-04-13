9a0fcaa8708885a53a1008bb87a62e35a2fec74c
= <undeliverable@example.com>
2024-04-13 18:18:58 +0900

    Copy imports in `alana.py` to `__init__.py`.
    `alana.py` may be deprecated in the future.
    
    Other minor changes.

--- alana/alana.py
+++ alana/alana.py
@@ -1,4 +1,3 @@
-from alana.color import log, red, blue, green, yellow, cyan
-import alana.prompt as prompt
+from alana.color import red, blue, green, yellow, cyan
 from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen
 from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot
