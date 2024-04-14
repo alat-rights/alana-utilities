07be7c812751d3a1b988e49120cd12251ad031d3
= <undeliverable@example.com>
2024-04-15 04:21:20 +0900

    `respond.py`

--- alana/__init__.py
+++ alana/__init__.py
@@ -1,5 +1,5 @@
 from alana.color import red, blue, green, yellow, cyan, pink
-from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt, pretty_print, remove_xml, gen_msg
+from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt, pretty_print, remove_xml, gen_msg, respond
 from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot, rm_xml
 import alana.globals
 import alana.color
