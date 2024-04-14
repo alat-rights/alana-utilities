4b8bf533292f2a3b71871730c9c64df04c0c43bc
= <undeliverable@example.com>
2024-04-15 08:00:02 +0900

    Moved README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # Read the contents of your README file
 with open("docs/README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+   long_description = fh.read()
 
 setup(
    name='alana',
