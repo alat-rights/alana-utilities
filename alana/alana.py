8f2a8160fd33c3ada70f637959acf0d6fa75e9c7
= <undeliverable@example.com>
2024-04-14 17:13:34 +0900

    Add alias rm_xml

--- alana/alana.py
+++ alana/alana.py
@@ -1,3 +1,3 @@
 from alana.color import red, blue, green, yellow, cyan
 from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt, pretty_print, remove_xml
-from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot
+from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot, rm_xml
