243d835c6412b7e59f49631325e932f16b0b9f4f
Domejko <sllawny@gmail.com>
2024-04-16 15:54:23 +0200

    Aliases moved to seperate file

--- alana/__init__.py
+++ alana/__init__.py
@@ -1,6 +1,6 @@
 from alana.color import red, blue, green, yellow, cyan, pink
 from alana.prompt import get_xml, get_xml_pattern, gen_examples, gen_examples_list, gen, gen_prompt, pretty_print, remove_xml, gen_msg, respond
-from alana.prompt import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot, rm_xml
+from alana.aliases import grab, xml, n_shot_list, n_shot, few_shot_list, few_shot, rm_xml
 import alana.globals
 import alana.color
 import alana.prompt
